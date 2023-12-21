df8a5f1adaee3a916b2c7cbd97ccd01dabc33139
dude <vmmgame84@gmail.com>
2023-12-20 23:32:01 -0600

    Dec 20 Final Changes

--- STA_REST_API/main/admin.py
+++ STA_REST_API/main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import TestResultModel, TestGroupModel
 
 # Register your models here.
+
+class TestResultAdmin(admin.ModelAdmin):
+    list_display = ['website_url', 'user', 'run_time']
+
+admin.site.register(TestResultModel, TestResultAdmin)
+
+class TestGroupAdmin(admin.ModelAdmin):
+    list_display = ['name','user']
+
+admin.site.register(TestGroupModel, TestGroupAdmin)
--- STA_REST_API/main/models.py
+++ STA_REST_API/main/models.py
@@ -6,11 +6,6 @@ from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 
-class Tester(User):
-    """An model that extends the Django user model"""
-    user_id = models.CharField(max_length=75) # This will be the user ID from givin from firebase
-    class Meta:
-        proxy = True
 
 class TestGroupModel(models.Model):
     """
@@ -19,20 +14,24 @@ class TestGroupModel(models.Model):
     name = models.CharField(max_length=25) # Name of the TestGroup # * Needs to be unique for the user
     file = models.FileField(validators=[
         FileExtensionValidator(['txt', 'csv']), ]) # Only accepts txt and csv files
-    user = models.ForeignKey('Tester', on_delete=models.CASCADE) # Link the user that owns this Test Group
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # Link the user that owns this Test Group
 
 class TestResultModel(models.Model):
     """
     This is a model that will store data from ran tests.
     Each Row will be a test
     """
-    website_name = models.CharField(max_length=250)  # The name of the website being tested.
-    user = models.ForeignKey('Tester', on_delete=models.CASCADE) # Link the user that owns this Test Group
-    test_group = models.ForeignKey('TestGroupModel', on_delete=models.SET_NULL, null=True) # Link to the test group
+    website_url = models.CharField(max_length=250)  # The url of the website being tested.
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # Link the user that owns this Test Group
+    test_group = models.ForeignKey(TestGroupModel, on_delete=models.SET_NULL, null=True) # Link to the test group
     run_time = models.FloatField() # The run time of the website in seconds
-    screenshot = models.ImageField() # ! Need to Fix
+
+    # ! Get Screenshots to work later
+    # screenshot = models.FilePathField(path="/screenshots/", null=True) # Will be the file path to the image
+    
+    
     errors = ArrayField(
-        base_field=models.TextField(max_length=200, null=True)
+        base_field=models.TextField(max_length=200, null=True, blank=True)
     )
-    webpage_size = models.FloatField() # The size of the webpage data in Mb
+    webpage_size = models.FloatField(null=True, blank=True) # The size of the webpage data in Mb
 
